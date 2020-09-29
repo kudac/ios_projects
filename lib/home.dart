ab98fb1155295a591bc0ffab7fe76b7668c538d3
kudac <kmutaurwa1@gmail.com>
2020-09-29 21:41:36 +0200

    my statementxxx

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+    return Container(
+      alignment:  Alignment.center,
+      color: Colors.deepPurple,
+      child: Text("FLIGHT" ,textDirection: TextDirection.ltr),
+    );
+  }
+
+
+
+}
